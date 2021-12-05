8b1e66293de7b376c5238c599f36d81334b0f620
shepard <eviltomorrow@163.com>
2021-12-05 11:46:44 +0800

    dev

--- internal/model/metadata.go
+++ internal/model/metadata.go
@@ -12,18 +12,18 @@ import (
 // Metadata trade data
 type Metadata struct {
 	ObjectID        string  `json:"_id" bson:"_id"`
-	Code            string  `json:"code"`
-	Name            string  `json:"name"`             // 0 股票简称
-	Open            float64 `json:"open"`             // 1 今日开盘价格
-	YesterdayClosed float64 `json:"yesterday-closed"` // 2 昨日收盘价格
-	Latest          float64 `json:"latest"`           // 3 最近成交价格
-	High            float64 `json:"high"`             // 4 最高成交价
-	Low             float64 `json:"low"`              // 5 最低成交价
-	Volume          uint64  `json:"volume"`           // 8 成交数量（股）
-	Account         float64 `json:"account"`          // 9 成交金额（元）
-	Date            string  `json:"date"`             // 30 日期
-	Time            string  `json:"time"`             // 31 时间
-	Suspend         string  `json:"suspend"`          // 32 停牌状态
+	Code            string  `json:"code" bson:"code"`
+	Name            string  `json:"name" bson:"name"`                         // 0 股票简称
+	Open            float64 `json:"open" bson:"open"`                         // 1 今日开盘价格
+	YesterdayClosed float64 `json:"yesterday_closed" bson:"yesterday_closed"` // 2 昨日收盘价格
+	Latest          float64 `json:"latest" bson:"latest"`                     // 3 最近成交价格
+	High            float64 `json:"high" bson:"high"`                         // 4 最高成交价
+	Low             float64 `json:"low" bson:"low"`                           // 5 最低成交价
+	Volume          uint64  `json:"volume" bson:"volume"`                     // 8 成交数量（股）
+	Account         float64 `json:"account" bson:"account"`                   // 9 成交金额（元）
+	Date            string  `json:"date" bson:"date"`                         // 30 日期
+	Time            string  `json:"time" bson:"time"`                         // 31 时间
+	Suspend         string  `json:"suspend" bson:"suspend"`                   // 32 停牌状态
 }
 
 func (m *Metadata) String() string {
